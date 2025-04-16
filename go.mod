5b3ba28036570c8bbb9a6f098e305fab6b2cd7a3
shadmanrakib <srakibnyc1@gmail.com>
2025-04-16 19:06:44 -0400

    initial work in generating jwt

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module sentinel
 
 go 1.23.5
+
+require github.com/golang-jwt/jwt/v5 v5.2.2
---
+++ utils.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claims = map[string]interface{}
+
+type UserData struct {
+	id         string
+	attributes Claims
+}
+
+type Identities = map[string]Claims
+
+func CreateIdToken(
+	secretKeyId string,
+	secretKey string,
+	issuer string,
+	sign_in_provider string,
+	userData UserData,
+	identities Identities,
+	authTime int,
+	tokenDurationInSeconds int,
+) (string, error) {
+	// TODO: fetch user data outside and pass in
+	// TODO: define a struct for this user data
+
+	expiredAt := authTime + tokenDurationInSeconds
+
+	var token = jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"typ":       "JWT",
+			"alg":       "HS256",
+			"kid":       secretKeyId,
+			"iss":       issuer,
+			"aud":       userData.id,
+			"auth_time": authTime,
+			"iat":       authTime,
+			"exp":       expiredAt,
+			"sub":       userData.id,
+			"sentinel": map[string]interface{}{
+				"identities":       identities,
+				"attributes":       userData.attributes,
+				"sign_in_provider": sign_in_provider,
+			},
+		})
+
+	var tokenStr, err = token.SignedString([]byte(secretKey))
+
+	return tokenStr, err
+}
+
+func CreateAccessToken(
+	secretKeyId string,
+	secretKey string,
+	issuer string,
+	signInProvider string,
+	userData UserData,
+	identities Identities,
+	scopes []string,
+	authTime int,
+	tokenDurationInSeconds int,
+) (string, error) {
+	// TODO: fetch user data outside and pass in
+	// TODO: define a struct for this user data
+
+	expiredAt := authTime + tokenDurationInSeconds
+
+	var token = jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"typ": "JWT",
+			"alg": "HS256",
+			"kid": secretKeyId,
+			"iss": issuer,
+			// TODO: maybe replace with url provided by user
+			"aud":       "sentinel",
+			"auth_time": authTime,
+			"iat":       authTime,
+			"exp":       expiredAt,
+			"sub":       userData.id,
+			"scopes":    []string{},
+		})
+
+	var tokenStr, err = token.SignedString([]byte(secretKey))
+
+	return tokenStr, err
+}
+
+func CreateRefreshToken() {
+
+}
