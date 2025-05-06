d8201ba5b6efc2d633d7a93154a3f462e003d621
shadmanrakib <srakibnyc1@gmail.com>
2025-05-06 18:02:40 -0400

    create identity model

---
+++ internal/models/identity.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Identity struct {
+	ID               string `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	ClientId         string `gorm:"not null"`
+	ProviderSub      string
+	ProviderOptionId string         `gorm:"type:varchar"`
+	ClientProviderId string         `gorm:"type:varchar"`
+	UserId           string         `gorm:"type:uuid"`
+	Data             JsonDictionary `gorm:"type:jsonb"`
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+	DeletedAt        gorm.DeletedAt `gorm:"index"`
+
+	Client         `gorm:"references:ID;foreignKey:ClientId"`
+	ProviderOption `gorm:"references:ID;foreignKey:ProviderOptionId"`
+	ClientProvider `gorm:"references:ID;foreignKey:ClientProviderId"`
+}
