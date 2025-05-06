9e8f3bf1878bfca34b3c30c74ea4e9bd9195036e
shadmanrakib <srakibnyc1@gmail.com>
2025-05-06 18:03:24 -0400

    swap client provider model to use uuid

--- internal/models/client_provider.go
+++ internal/models/client_provider.go
@@ -7,7 +7,7 @@ import (
 )
 
 type ClientProvider struct {
-	ID               string         `gorm:"type:varchar;primaryKey"`
+	ID               string         `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
 	ClientId         string         `gorm:"not null"`
 	ProviderOptionId string         `gorm:"type:varchar"`
 	Data             JsonDictionary `gorm:"type:jsonb"`
