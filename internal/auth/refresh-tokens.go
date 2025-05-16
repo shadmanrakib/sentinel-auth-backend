e191c5ee81f61a6906f6e2ed3810ce2a46973740
shadmanrakib <srakibnyc1@gmail.com>
2025-05-16 17:29:05 -0400

    restrict token to client id

---
+++ internal/auth/refresh-tokens.go
@@ -0,0 +1,84 @@
+package auth
+
+import (
+	"errors"
+	"sentinel-auth-backend/internal/crypto"
+	"sentinel-auth-backend/internal/models"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type RefreshTokensWithRefreshTokenError string
+
+const (
+	RefreshTokensWithRefreshTokenErrorInvalidToken RefreshTokensWithRefreshTokenError = "invalid refresh token"
+)
+
+type RefreshedTokens struct {
+	Access    string
+	Id        string
+	ExpiresIn int
+}
+
+func getRefreshTokenByToken(db *gorm.DB, token string) (*models.RefreshToken, error) {
+	var rf models.RefreshToken
+	result := db.First(&rf, "token = ?", token)
+
+	if result.RowsAffected == 0 {
+		return nil, errors.New("failed to find")
+	}
+
+	return &rf, nil
+}
+
+func RefreshTokensWithRefreshToken(db *gorm.DB, clientId string, token string) (*RefreshedTokens, error) {
+	rf, err := getRefreshTokenByToken(db, token)
+	if err != nil || rf.ClientId != clientId {
+		return nil, errors.New(string(RefreshTokensWithRefreshTokenErrorInvalidToken))
+	}
+
+	now := time.Now()
+	hasExpired := rf.ExpiresAt.Unix() <= now.Unix()
+	if hasExpired || rf.Revoked {
+		return nil, errors.New(string(RefreshTokensWithRefreshTokenErrorInvalidToken))
+	}
+
+	shortTokenDurationSeconds := 60 * 60 * 1
+	accessToken, err := crypto.CreateAccessToken(
+		rf.Client.ID,
+		rf.Client.Secret,
+		"",
+		rf.Identity.ProviderOptionId,
+		crypto.UserData{},
+		crypto.Identities{},
+		[]string{"profile"},
+		now.Unix(),
+		shortTokenDurationSeconds,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	idToken, err := crypto.CreateIdToken(
+		rf.Client.ID,
+		rf.Client.Secret,
+		"",
+		rf.Identity.ProviderOptionId,
+		crypto.UserData{},
+		crypto.Identities{},
+		now.Unix(),
+		shortTokenDurationSeconds,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	tokens := RefreshedTokens{
+		Access:    accessToken,
+		Id:        idToken,
+		ExpiresIn: shortTokenDurationSeconds,
+	}
+
+	return &tokens, nil
+}
